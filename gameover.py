e635f67eb68501604d9acc97b58d412bb0be64d8
Lorence Jugz <119779367+KalampagDiwa@users.noreply.github.com>
2023-09-22 14:00:03 +1200

    Add files via upload

---
+++ gameover.py
@@ -0,0 +1,38 @@
+#learning Activity 25 -Mastering Python Print
+#game over
+#@ author: LorenceJuguilon
+# Date 17/08/2023
+
+print ("program 'Game Over' 2.0")
+
+print ("Same", "message" , "as before")
+
+print ("Just",
+        "a bit",
+        "bigger")
+
+print ("Here", end=" ")
+print ("it is...")
+
+print (
+        """
+
+                _____       ___       ____  ___   _____
+               /  ___|     /   |     /    |/   | |  ___|
+               | |        / /| |    /   |   /| | |  |__  
+               | |  _    / __  |   /  / |__/ | | |   __|
+               | |_| |  / /  | |  /  /       | | |  |__
+               \_____/ /_/   |_| /__/        |_| |_____|
+
+                _____   _     _   ____   _____
+               /  _  \ | |   / / | ___| |  _  \
+               | | | | | |  / /  | |__  | |_| |
+               | | | | | | / /   |  __| |  _
+               | | | | | |/ /    | |__  | | \ \
+               \_____/ |___/     |____| |_|  \_\
+
+
+                                                              """
+        )
+
+input ( "\n\nPress the enter key to exit.")
